fe4d53291b7cea798500e10dbc45b7e99d04a791
eva-matus <eva.matus@hotmail.com>
2021-02-25 21:48:37 -0300

    inicializando el telefono

--- celular/celular.js
+++ celular/celular.js
@@ -11,8 +11,35 @@ class celulares {
         this.resolucionDePantalla = rdp;
         this.resolucionDeCamara = rdc;
         this.memoriaRam = ram;
+        this.encendido = false;
     }
-    prender(){
-        
+    presionarBotonEncendido(){
+        if (this.encendido == false){
+            alert("celular apagado");
+            this.encendido = true;
+        } else {
+            alert("el celular ya esta encendido")
+        }
+    }
+    reiniciar(){
+        if (this.encendido == true){
+            alert("reiniciar celular");
+        } else {
+            alert("el celular esta apagado")
+        }
+    }
+    tomarFoto(){
+        alert(`foto tomada en una resolucion de: ${this.resolucionDeCamara}`)
+    }
+    grabandoVideo(){
+        alert(`grabando video en ${this.resolucionDeCamara}`)
     }
 }
+
+celular1 = new celulares("rojo", "150gr", "5'", "full HD", "2GB");
+
+celular1.presionarBotonEncendido();
+celular1.tomarFoto();
+celular1.grabandoVideo();
+celular1.reiniciar();
+celular1.presionarBotonEncendido();
