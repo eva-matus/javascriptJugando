39011b0483b7bc7e38df8dd2bd8890931c9daee0
eva-matus <eva.matus@hotmail.com>
2021-02-25 21:24:50 -0300

    sistema que permita comprar un celular con tres opciones para un ciente inepto

---
+++ celular/celular.js
@@ -0,0 +1,18 @@
+// Juana necesita comprar un celular, pero no sabe por cual de tres
+// modelos escoger.
+// - Crear un sistema para mostrarle a Juana las particularidades de los 3 celulares. 
+// - Cada celular debe tener color, peso, resolucion de pantalla, resolucion de camara y memoria
+// - Cada Celular debe poder prender, reiniciar, apagar, tomar fotos y grabar
+
+class celulares {
+    constructor(color, peso, rdp, rdc, ram){
+        this.color = color;
+        this.peso = peso;
+        this.resolucionDePantalla = rdp;
+        this.resolucionDeCamara = rdc;
+        this.memoriaRam = ram;
+    }
+    prender(){
+        
+    }
+}
